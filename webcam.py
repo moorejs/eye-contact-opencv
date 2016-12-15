fe27f7e5219123a90ac852d2668a6916600c185e
Jared Moore <jmoore1@andrew.cmu.edu>
2016-12-14 22:18:55 -0500

    Added Shepard's Tone loop

--- webcam.py
+++ webcam.py
@@ -1,3 +1,5 @@
+import Queue
+
 import numpy as np
 import cv2
 
@@ -10,40 +12,48 @@ if not fluidsynth.init(SF2):
     import sys
     print('Failed to load {}'.format(SF2))
     sys.exit(1)
-else:
-    print('found')
 
-x_offset = 480
-y_offset = 30
-height = 720 - 2 * y_offset - 100
-width = 1280 - 2 * x_offset
+print('FluidSynth initialized')
+
+INPUT_WIDTH = 1920
+INPUT_HEIGHT = 1080
+
+x_offset = int(0.40 * INPUT_WIDTH)
+y_offset = int(0.30 * INPUT_HEIGHT)
+height = INPUT_HEIGHT - 2 * y_offset
+width = INPUT_WIDTH - 2 * x_offset
 
 cam = cv2.VideoCapture(0)
 
 detect_timer = 0
-DETECT_WAIT = 10
+DETECT_WAIT = 3
 
 has_user = True  # debug
 has_user_counter = 0
 has_user_timeout = 0
-HAS_USER_WAIT = 50
+HAS_USER_WAIT = 10000
 
-NOTE_NAMES = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
-NOTE_COUNT = 7
-notes = NoteContainer([Note("A", 4), Note("D", 4), Note("G", 4)])
+BASE_OCTAVE = 5
+base_note = int(Note('C', BASE_OCTAVE))
+notes = NoteContainer()
+
+NOTE_COUNT = 6
+NOTE_SPACING = 3
+for offset in range(0, NOTE_SPACING * NOTE_COUNT, NOTE_SPACING):
+    note = Note().from_int(base_note + offset)
+    note.next_octave = offset >= 12
+    notes += note
 
 if cam.isOpened():
     while True:
         ret, frame = cam.read()
         frame = frame[y_offset:y_offset + height, x_offset:x_offset + width]
         if ret:
-            print("Has user: ", has_user, has_user_counter,
-                  "timeout timer", has_user_timeout)
+            # print("Has user: ", has_user, has_user_counter,
+            #      "timeout timer", has_user_timeout)
 
             detect_timer += 1
 
-            # frame = cv2.cvtColor(cv2.resize(
-            #    frame, size), cv2.COLOR_BGR2GRAY)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             faces = cv2.CascadeClassifier('haarcascade_eye.xml')
@@ -62,18 +72,15 @@ if cam.isOpened():
 
                     fluidsynth.play_NoteContainer(notes)
 
-                    new_notes = NoteContainer()
                     for note in notes:
-                        # note.set_note
-                        new_note = note.from_int((int(note) + 1) % 12)
-                        new_notes.add_note(new_note)
-                       # notes.add_note(new_note)
-                       # notes.remove_note(note)
-                        # note.octave_up()
-                    #    # note.set_note()
-                    #    note.octave_up()
-                    print(new_notes)
-                    notes = new_notes
+                        print(note, int(note), note.next_octave,
+                              (BASE_OCTAVE + note.next_octave) * 12)
+                        new_note = (int(note) + 1) % 12
+                        if new_note == 0:
+                            note.next_octave = not note.next_octave
+                        note = note.from_int(
+                            (BASE_OCTAVE + note.next_octave) * 12 + new_note)
+
             else:
                 has_user_counter += 1
                 has_user_timeout = 0
