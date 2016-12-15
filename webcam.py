f986f89afbaed104f2fc68920c717d07ae48fbd6
Jared Moore <jmoore1@andrew.cmu.edu>
2016-12-15 13:28:48 -0500

    Final

--- webcam.py
+++ webcam.py
@@ -15,23 +15,23 @@ if not fluidsynth.init(SF2):
 
 print('FluidSynth initialized')
 
-INPUT_WIDTH = 1920
-INPUT_HEIGHT = 1080
+TARGET_WIDTH = 1280
+TARGET_HEIGHT = 720
 
-x_offset = int(0.40 * INPUT_WIDTH)
-y_offset = int(0.30 * INPUT_HEIGHT)
-height = INPUT_HEIGHT - 2 * y_offset
-width = INPUT_WIDTH - 2 * x_offset
+x_offset = int(0.4 * TARGET_WIDTH)
+y_offset = int(0.3 * TARGET_HEIGHT)
+height = TARGET_HEIGHT - 2 * y_offset - 60  # remove torso
+width = TARGET_WIDTH - 2 * x_offset
 
 cam = cv2.VideoCapture(0)
 
 detect_timer = 0
-DETECT_WAIT = 3
+DETECT_WAIT = 9
 
-has_user = True  # debug
+has_user = False
 has_user_counter = 0
 has_user_timeout = 0
-HAS_USER_WAIT = 10000
+HAS_USER_WAIT = 50
 
 BASE_OCTAVE = 5
 base_note = int(Note('C', BASE_OCTAVE))
@@ -44,20 +44,22 @@ for offset in range(0, NOTE_SPACING * NOTE_COUNT, NOTE_SPACING):
     note.next_octave = offset >= 12
     notes += note
 
+eyes = cv2.CascadeClassifier('haarcascade_eye.xml')
+
 if cam.isOpened():
     while True:
         ret, frame = cam.read()
+        frame = cv2.resize(frame, (TARGET_WIDTH, TARGET_HEIGHT))
         frame = frame[y_offset:y_offset + height, x_offset:x_offset + width]
         if ret:
-            # print("Has user: ", has_user, has_user_counter,
-            #      "timeout timer", has_user_timeout)
+            print("Has user: ", has_user, has_user_counter,
+                  "timeout timer", has_user_timeout)
 
             detect_timer += 1
 
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            faces = cv2.CascadeClassifier('haarcascade_eye.xml')
-            detected = faces.detectMultiScale(frame, 1.3, 5)
+            detected = eyes.detectMultiScale(frame, 1.3, 5)  # 1.3, 5
 
             if not len(detected):
                 has_user_counter = 0
